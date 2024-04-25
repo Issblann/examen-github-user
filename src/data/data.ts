f3be24e6addd47dd61b08f80b2f5fe5fa2ba5563
Issblann <anaisabelpp@outlook.com>
2024-04-25 14:13:56 -0500

    add promise to fetch technologies by repo

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,16 +1,30 @@
 import axios from 'axios';
 
-const URL_BASE = 'https://api.github.com/users';
+//URL base del usuario de la API de Github
+const URL_BASE_USER = 'https://api.github.com/users';
 
+const URL_BASE_REPO = 'https://api.github.com/repos';
+
+//Peticion usuario
 export const getUser = async (username: string | null) => {
-  const response = await axios.get(`${URL_BASE}/${username}`);
+  const response = await axios.get(`${URL_BASE_USER}/${username}`);
   const user = response.data;
   return user;
   //   console.log(user);
 };
 
-export const getRepos = async (username: string) => {
-  const response = await axios.get(`${URL_BASE}/${username}/repos`);
+//Peticion repositorios
+export const getRepos = async (username: string | null) => {
+  const response = await axios.get(`${URL_BASE_USER}/${username}/repos`);
   const recentRepos = response.data.slice(0, 5);
-  console.log(recentRepos);
+
+  const reposWithLanguages = await Promise.all(
+    recentRepos.map(async (repo: any) => {
+      const languagesResponse = await axios.get(
+        `${URL_BASE_REPO}/${username}/${repo?.name}/languages`
+      );
+      return { ...repo, languages: languagesResponse.data };
+    })
+  );
+  return reposWithLanguages;
 };
